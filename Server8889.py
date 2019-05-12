736810a1a72897c450d67f6cdbda2f7f0e5934ae
Huasheng <huashengwang1989@gmail.com>
2019-05-12 23:00:04 +0800

    Server8889 python script would fallback to the node_module path for cert as original, if no argvs specified

--- Server8889.py
+++ Server8889.py
@@ -3,49 +3,52 @@ from optparse import OptionParser
 import ssl
 import sys
 
-class RequestHandler(BaseHTTPRequestHandler):    
-    def do_GET(self):
-        
-        request_path = self.path
-        
-        print("\n----- Request Start ----->\n")
-        print(request_path)
-        print(self.headers)
-        print("<----- Request End -----\n")
-        
-        self.send_response(200)
-        self.send_header("Set-Cookie", "foo=bar")
-        self.send_header()
-    
-    def do_POST(self):
-        
-        request_path = self.path
-        
-        print("\n----- Request Start ----->\n")
-        print(request_path)
-        
-        request_headers = self.headers
-        content_length = request_headers.getheaders('content-length')
-        length = int(content_length[0]) if content_length else 0
-        
-        print(request_headers)
-        print(self.rfile.read(length))
-        print(self)
-        print("<----- Request End -----\n")
-        
-        self.send_response(200)
-    
-    do_PUT = do_POST
-    do_DELETE = do_GET
+class RequestHandler(BaseHTTPRequestHandler):	
+	def do_GET(self):
+		
+		request_path = self.path
+		
+		print("\n----- Request Start ----->\n")
+		print(request_path)
+		print(self.headers)
+		print("<----- Request End -----\n")
+		
+		self.send_response(200)
+		self.send_header("Set-Cookie", "foo=bar")
+		self.send_header()
+	
+	def do_POST(self):
+		
+		request_path = self.path
+		
+		print("\n----- Request Start ----->\n")
+		print(request_path)
+		
+		request_headers = self.headers
+		content_length = request_headers.getheaders('content-length')
+		length = int(content_length[0]) if content_length else 0
+		
+		print(request_headers)
+		print(self.rfile.read(length))
+		print(self)
+		print("<----- Request End -----\n")
+		
+		self.send_response(200)
+	
+	do_PUT = do_POST
+	do_DELETE = do_GET
 
 def main():
-    port = 8889
-    print('Listening on localhost:%s' % port)
-    server = HTTPServer(('', port), RequestHandler)
-    server.socket = ssl.wrap_socket(server.socket, certfile = sys.argv[1], server_side=True)
-    server.serve_forever()
-
+	port = 8889
+	print('Listening on localhost:%s' % port)
+	server = HTTPServer(('', port), RequestHandler)
+	CERTFILE = '/usr/local/lib/node_modules/homebridge-samsung-aircon-8888/ac14k_m.pem'
+	if (len(sys.argv) > 1):
+		CERTFILE = sys.argv[1]
+	print("certfile: ", CERTFILE)
+	server.socket = ssl.wrap_socket(server.socket, certfile = CERTFILE, server_side = True)
+	server.serve_forever()
 
 if __name__ == "__main__":
-                    
-    main()
+				
+	main()
